c889adde0c03bcc72d6e4c829c0eb917bfd1afcc
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2025-11-30 15:20:21 +0530

    Added Wrapper class and Object Class example code

---
+++ src/main/java/OOPS/ObjectClass/Person.java
@@ -0,0 +1,15 @@
+package OOPS.ObjectClass;
+
+class Person {
+    String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person name: " + name;
+    }
+}
+
