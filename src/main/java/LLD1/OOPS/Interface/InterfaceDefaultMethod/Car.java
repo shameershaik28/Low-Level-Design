a5a0549cc185d5927d4cc454d71b1cc70fc78006
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2025-12-25 15:22:16 +0530

    Added default method

--- src/main/java/LLD1/OOPS/Interface/InterfaceDefaultMethod/Car.java
+++ src/main/java/LLD1/OOPS/Interface/InterfaceDefaultMethod/Car.java
@@ -1,6 +1,11 @@
 package LLD1.OOPS.Interface.InterfaceDefaultMethod;
 
 class Car implements Vehicle {
+
+    @Override
+    public void stop() { //default method from interface is overridden here
+        System.out.println("Car stopped");
+    }
     public void start() {
         System.out.println("Car starting...");
     }
